cc60ef7829440ce9bf21f271e3847501e1990903
Guilherme Berger <guilherme.berger@gmail.com>
2014-07-02 22:47:54 -0300

    closure

--- script/app.js
+++ script/app.js
@@ -1,110 +1,117 @@
-/* UTILS
- *******/
+(function($, Rx, window) {
+  'use strict';
 
-var shuffle = function(array) {
-  var arr = _.clone(array);
-  var currentIndex = arr.length;
-  var randomIndex;
-  var tmp;
+  var cities = window.cities;
 
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+  /* UTILS
+   *******/
 
-    tmp = arr[currentIndex];
-    arr[currentIndex] = arr[randomIndex];
-    arr[randomIndex] = tmp;
-  }
+  var shuffle = function (array) {
+    var arr = _.clone(array);
+    var currentIndex = arr.length;
+    var randomIndex;
+    var tmp;
 
-  return arr;
-};
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-var sample = function(arr, n) {
-  if(isNaN(n)){
-    n = 1;
-  }
-  var shuffled = shuffle(arr);
-  return n === 1 ? shuffled[0] : shuffled.slice(0, n);
-};
+      tmp = arr[currentIndex];
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = tmp;
+    }
 
-var buildUrl = function(city) {
-  return "img/cities/" + city.id + ".png";
-};
+    return arr;
+  };
+
+  var sample = function (arr, n) {
+    if (isNaN(n)) {
+      n = 1;
+    }
+    var shuffled = shuffle(arr);
+    return n === 1 ? shuffled[0] : shuffled.slice(0, n);
+  };
+
+  var buildUrl = function (city) {
+    return "img/cities/" + city.id + ".png";
+  };
 
 
-/* JQUERY
- ********/
+  /* JQUERY
+   ********/
 
-var $correct = $('.correct');
-var $total = $('.total');
-var $buttons = $('.choice button');
-var $choice1 = $('#choice-1');
-var $choice2 = $('#choice-2');
-var $choice3 = $('#choice-3');
-var $mapImg = $('#map-img');
-var $refreshButton = $('.refresh-button');
+  var $correct = $('.correct');
+  var $total = $('.total');
+  var $buttons = $('.choice button');
+  var $choice1 = $('#choice-1');
+  var $choice2 = $('#choice-2');
+  var $choice3 = $('#choice-3');
+  var $mapImg = $('#map-img');
+  var $refreshButton = $('.refresh-button');
 
-var elementFromCityId = function(cityId) {
-  return $buttons.filter(function(i, el) {
-    return $(el).data('city-id') === cityId;
+  var elementFromCityId = function (cityId) {
+    return $buttons.filter(function (i, el) {
+      return $(el).data('city-id') === cityId;
+    });
+  };
+
+
+  /* RX.JS
+   *******/
+
+  var resultStream = Rx.Observable.fromEvent($buttons, 'click')
+    .map(function (e) {
+      var guess = $(e.target).data('city-id');
+      var actual = $mapImg.data('city-id');
+      return {
+        guess: guess,
+        actual: actual,
+        correctness: guess === actual,
+        guessElement: elementFromCityId(guess),
+        actualElement: elementFromCityId(actual)
+      };
+    });
+
+  var correctStream = resultStream.scan(0, function (acc, result) {
+    return acc + (result.correctness ? 1 : 0);
   });
-};
-
-
-/* RX.JS
- *******/
-
-var resultStream = Rx.Observable.fromEvent($buttons, 'click')
-  .map(function(e) {
-    var guess = $(e.target).data('city-id');
-    var actual = $mapImg.data('city-id');
-    return {
-      guess: guess,
-      actual: actual,
-      correctness: guess === actual,
-      guessElement: elementFromCityId(guess),
-      actualElement: elementFromCityId(actual)
-    };
+  correctStream.subscribe(function (correct) {
+    $correct.text(correct);
   });
 
-var correctStream = resultStream.scan(0, function(acc, result) {
-  return acc + (result.correctness ? 1 : 0);
-});
-correctStream.subscribe(function(correct) {
-  $correct.text(correct);
-});
+  var totalStream = resultStream.scan(0, function (acc) {
+    return acc + 1;
+  });
+  totalStream.subscribe(function (total) {
+    $total.text(total);
+  });
 
-var totalStream = resultStream.scan(0, function(acc) {
-  return acc + 1;
-});
-totalStream.subscribe(function(total) {
-  $total.text(total);
-});
+  resultStream.subscribe(function (result) {
+    console.log(result);
+    $buttons.attr('disabled', true);
 
-resultStream.subscribe(function(result) {
-  console.log(result);
-  $buttons.attr('disabled', true);
+    if (result.correctness) {
+      result.guessElement.addClass("guess-correct");
+    } else {
+      result.actualElement.addClass("guess-correct");
+      result.guessElement.addClass("guess-wrong");
+    }
+  });
 
-  if(result.correctness) {
-    result.guessElement.addClass("guess-correct");
-  } else {
-    result.actualElement.addClass("guess-correct");
-    result.guessElement.addClass("guess-wrong");
-  }
-});
+  var refreshStream = Rx.Observable.fromEvent($refreshButton, 'click').startWith('startup');
 
-var refreshStream = Rx.Observable.fromEvent($refreshButton, 'click').startWith('startup');
+  refreshStream.subscribe(function () {
+    $buttons.removeClass("guess-correct guess-wrong").attr('disabled', false);
 
-refreshStream.subscribe(function(){
-  $buttons.removeClass("guess-correct guess-wrong").attr('disabled', false);
+    var possible = sample(cities, 3);
+    var chosen = sample(possible);
+    $choice1.text(possible[0].name).data('city-id', possible[0].id);
+    $choice2.text(possible[1].name).data('city-id', possible[1].id);
+    $choice3.text(possible[2].name).data('city-id', possible[2].id);
+    $mapImg.attr('src', 'img/blank.gif');
+    setTimeout(function () {
+      $mapImg.attr('src', buildUrl(chosen)).data('city-id', chosen.id);
+    }, 0);
+  });
 
-  var possible = sample(cities, 3);
-  var chosen = sample(possible);
-  $choice1.text(possible[0].name).data('city-id', possible[0].id);
-  $choice2.text(possible[1].name).data('city-id', possible[1].id);
-  $choice3.text(possible[2].name).data('city-id', possible[2].id);
-  $mapImg.attr('src', 'img/blank.gif');
-  setTimeout(function(){
-    $mapImg.attr('src', buildUrl(chosen)).data('city-id', chosen.id);
-  }, 0);
-});
+})(jQuery, Rx, window);
